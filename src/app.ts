602b72c21f63e16a51685bb21844e3fe98bafd48
Angélica Resende <angelica.resende@ufenesp.com.br>
2023-12-13 10:20:42 -0300

    alteração rota

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from '@/http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data:{
-    name: 'Angélica Resende',
-    email: 'angelresende007@gmail.com'
-  }
-})
+app.register(appRoutes)
