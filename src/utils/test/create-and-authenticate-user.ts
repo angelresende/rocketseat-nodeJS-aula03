6d3d06075b2ad399357ef17b4c1a098cf0fcd072
Angélica Resende <70111126+angelresende@users.noreply.github.com>
2024-01-23 15:28:23 -0300

    tokens

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -3,13 +3,13 @@ import request from 'supertest'
 
 export async function createAndAuthenticateUser(app: FastifyInstance) {
   await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
+    name: 'Maria da Silva',
+    email: 'maria.silva@email.com',
     password: '123456',
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
-    email: 'johndoe@example.com',
+    email: 'maria.silva@email.com',
     password: '123456',
   })
 
